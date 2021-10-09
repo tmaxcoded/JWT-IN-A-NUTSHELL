e198da4088eb24e8f4109b8c60b9b04b5f9a8b88
tmaxcoded <tmaxcoded@outlook.com>
2021-10-09 19:23:34 +0100

    new changes

--- chat-app/js/services.js
+++ chat-app/js/services.js
@@ -1,47 +1,4 @@
 
-let appendHtml = () =>{
-    element.appendChild(llist)
-}
-
-
-
-let mapUsers = (dataArray) => {
-   
-    dataArray.map(user => {
-        let {email,firstName,lastName,id,phoneNumber,userName}= user;
-        console.log(email,firstName,id)
-        
-       appendHtml()
-       // appendHtmlNew();
-       
-        
-            
-    })
-
-}
-
-
-let getAllUsers = () => {
-
-    axios.get("https://localhost:44345/api/authentication/users")
-     .then(res => {
-        console.log("mressage sent with response", res)
-        let {status,data} = res;
-        if(status === 200){
-            // store in local db
-            mapUsers(data.result)
-            
-            console.log(email,firstName,id)
-            
-            
-        }
-     })
-     .catch(err => {
-         console.log("error from server", err)
-     })
-}
-
-
 
 let element = document.getElementById("ui-list")
 let llist = document.createElement("li");
@@ -56,107 +13,56 @@ let createParagraphForUserInfo = document.createElement("p");
 createDflexDiv.className = "d-flex bd-highlight";
 createImageConentDiv.className = "img_cont";
 createUserInfoDiv.className = "user_info";
+createUserInfoSpan.innerHTML ="Sami Rafi";
 createUserInfoSpan.className ="info-span";
 creatImageDivs.className ="rounded-circle user_img";
 createOnlineUserInfoDiv.className ="online_icon";
+createParagraphForUserInfo.innerHTML ="Sami is online"
 
 creatImageDivs.src = "https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg";
 
 
+let getAllUsers = () => {
+
+    axios.get("https://localhost:44345/api/authentication/users")
+     .then(res => {
+        console.log("mressage sent with response", res)
+        let {status,data} = res;
+        if(status === 200){
+            // store in local db
+            mapUsers(data.result)
+            
+           // console.log(email,firstName,id)
+            
+            
+        }
+     })
+     .catch(err => {
+         console.log("error from server", err)
+     })
+}
 
 
 
-// let appendHtml = () => {
-    
-//     let element = document.getElementById("ui-list")
-//     let elementchild = document.getElementById("img_cont_id")
-
-//     element.appendChild(LIElement())
-//     .appendChild(DivElementWithDFlexAtrributes())
-//     .append(DivElementWithImag_contAtrributes(),DivElementWithUserInfoAtrributes())
-//     .appendChild(ImageElementWithAtrributes())
-
-// }
-
-// let appendHtmlNew = (elementchild) => {
-    
-//     elementchild.appendChild(ImageElementWithAtrributes())
-
-//     console.log("yaya", elementchild);
-// }
-
-// let LIElement = () => {
-//     return document.createElement("li")
-// } 
-
-// let DivElementWithDFlexAtrributes = () => {
-//     divElement = document.createElement("div");
-
-//     divElement.className="d-flex bd-highlight"
-
-//     divElement.id = "d-flex-id"
-
-    
-
-//     return divElement;
-    
-// } 
+let mapUsers = (dataArray) => {
+    dataArray.forEach(user => {
+       appendHtml()
+    })
+}
 
 
-// let DivElementWithImag_contAtrributes = () => {
-//     divElement = document.createElement("div");
+let appendHtml = () =>{
+    element.appendChild(llist)
+    llist.appendChild(createDflexDiv)
+    createDflexDiv.append(createImageConentDiv,createUserInfoDiv)
+    createImageConentDiv.append(creatImageDivs,createOnlineUserInfoDiv)
+    createUserInfoDiv.append(createUserInfoSpan,createParagraphForUserInfo)
+}
 
-//     divElement.className="img_cont"
-//     divElement.id ="img_cont_id"
-   
+
+
    
 
-//     return divElement;
-    
-// } 
-
-
-// let DivElementWithUserInfoAtrributes = () => {
-//     divElement = document.createElement("div");
-
-//     divElement.className="user_info"
-   
-   
-
-//     return divElement;
-    
-// } 
-
-// let UserInformationSpan = () => {
-//     divElement = document.createElement("span");
-
-//     divElement.innerText="kunle"
-   
-   
-
-//     return divElement;
-// }
-
-// let ImageElementWithAtrributes = () => {
-//     divElement = document.createElement("img");
-
-//     divElement.className="rounded-circle user_img"
-//     divElement.src ="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg"
-
-
-//     divElement
-//     return divElement;
-    
-// } 
-
-// let  SpanElementWithAtrributes = () => {
-//     divElement = document.createElement("span");
-
-//     divElement.className="online_icon"
-
-//     return divElement;
-    
-// } 
 
 
 
