5282057f97e03ff33bdfa1f34d31d84e412c11b2
tmaxcoded <tmaxcoded@outlook.com>
2021-10-09 23:11:37 +0100

    new changes

--- chat-app/js/services.js
+++ chat-app/js/services.js
@@ -1,26 +1,7 @@
 
 
-let element = document.getElementById("ui-list")
-let llist = document.createElement("li");
-let createDflexDiv = document.createElement("div");
-let createImageConentDiv = document.createElement("div");
-let createUserInfoDiv = document.createElement("div");
-let createUserInfoSpan = document.createElement("span");
-let creatImageDivs = document.createElement("img");
-let createOnlineUserInfoDiv = document.createElement("span");
-let createParagraphForUserInfo = document.createElement("p");
 
-createDflexDiv.className = "d-flex bd-highlight";
-createImageConentDiv.className = "img_cont";
-createUserInfoDiv.className = "user_info";
-createUserInfoSpan.innerHTML ="Sami Rafi";
-createUserInfoSpan.className ="info-span";
-creatImageDivs.className ="rounded-circle user_img";
-createOnlineUserInfoDiv.className ="online_icon";
-createParagraphForUserInfo.innerHTML ="Sami is online"
-
-creatImageDivs.src = "https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg";
-
+var _userId = '';
 
 let getAllUsers = () => {
 
@@ -31,9 +12,10 @@ let getAllUsers = () => {
         if(status === 200){
             // store in local db
             mapUsers(data.result)
+            let retrievedUserId = getIndividualClickedImages();
             
-           // console.log(email,firstName,id)
-            
+           
+           
             
         }
      })
@@ -42,22 +24,80 @@ let getAllUsers = () => {
      })
 }
 
+let appendHtml = (user) =>{
+    let element = document.getElementById("ui-list")
+let llist = document.createElement("li");
+let createDflexDiv = document.createElement("div");
+let createImageConentDiv = document.createElement("div");
+let createUserInfoDiv = document.createElement("div");
+let createUserInfoSpan = document.createElement("span");
+let creatImageDivs = document.createElement("img");
+let createOnlineUserInfoDiv = document.createElement("span");
+let createParagraphForUserInfo = document.createElement("p");
 
 
-let mapUsers = (dataArray) => {
-    dataArray.forEach(user => {
-       appendHtml()
-    })
-}
+createDflexDiv.className = "d-flex bd-highlight";
+createImageConentDiv.className = "img_cont";
+createUserInfoDiv.className = "user_info";
+createUserInfoSpan.innerHTML =user.firstName;
+createUserInfoSpan.className ="info-span";
+creatImageDivs.className ="rounded-circle user_img";
+creatImageDivs.id="id_user_img";
+createOnlineUserInfoDiv.className ="online_icon";
+createParagraphForUserInfo.innerHTML =`${user.firstName} is online`;
+
+creatImageDivs.src = user.imgUrl;
+creatImageDivs.setAttribute("user-id",user.id)
+creatImageDivs.setAttribute("first-name",user.firstName)
+creatImageDivs.setAttribute("user-image-url",user.imgUrl)
 
 
-let appendHtml = () =>{
     element.appendChild(llist)
     llist.appendChild(createDflexDiv)
     createDflexDiv.append(createImageConentDiv,createUserInfoDiv)
     createImageConentDiv.append(creatImageDivs,createOnlineUserInfoDiv)
     createUserInfoDiv.append(createUserInfoSpan,createParagraphForUserInfo)
 }
+
+let mapUsers = (dataArray) => {
+    dataArray.forEach(user => {
+        console.log(user)
+       appendHtml(user)
+    })
+}
+
+
+let getIndividualClickedImages = () => {
+
+    //let _userId = '';
+    // console.log(email,firstName,id)
+    let allImages = document.querySelectorAll('#id_user_img')
+          
+
+    allImages.forEach(img => {
+     console.log(img)
+        img.addEventListener('click', (e)=> {
+            e.preventDefault();
+            //alert("you")
+            //console.log(e.target.getAttribute("user-id")); whom-am-chatting-img
+            document.querySelector("#whom-am-chatting > span").innerHTML = e.target.getAttribute("first-name")
+            document.querySelector("#whom-am-chatting-img > img").src = e.target.getAttribute("user-image-url")
+           // changeChatProfile.ine
+            _userId = e.target.getAttribute("user-id")
+
+        })
+    })
+    console.log('retrievedUserId',_userId);
+    //return _userId;
+}
+
+
+
+
+
+
+
+
 
 
 
